12e57468481b0f137172f07130b5b1d6332058a9
Phan Quan <quan.infotech@gmail.com>
2015-09-25 13:46:34 +0700

    upload project

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,45 @@
+class UsersController < ApplicationController
+  before_filter :set_user, only: [:edit, :update, :destroy]
+  def index
+    @users = User.all
+  end
+
+  # Create new users
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to users_path
+    end
+  end
+
+  # Update User's information
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    if @user = User.find(params[:id]).update(user_params)
+      redirect_to users_path
+    end
+  end
+
+  # Delete User
+  def destroy
+    @user = User.find(params[:id]).destroy
+    redirect_to users_path
+  end
+
+  private
+  # Set user's variable
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  def user_params
+    params.require(:user).permit(:name, :email, :encrypted_password, :role)
+  end
+end
