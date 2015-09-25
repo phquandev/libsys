33a5a34fc4aeeb550feb99b8ebf35bf3bcaa6a30
Phan Quan <quan.infotech@gmail.com>
2015-09-25 13:50:39 +0700

    Change datatype of role

--- db/migrate/20150925040438_add_properties_to_users.rb
+++ db/migrate/20150925040438_add_properties_to_users.rb
@@ -1,6 +1,6 @@
 class AddPropertiesToUsers < ActiveRecord::Migration
   def change
     add_column :users, :name, :string
-    add_column :users, :role, :string
+    add_column :users, :role, :integer
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -27,7 +27,7 @@ ActiveRecord::Schema.define(version: 20150925040438) do
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
     t.string   "name"
-    t.string   "role"
+    t.integer  "role"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
